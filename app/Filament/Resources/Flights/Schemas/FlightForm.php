76c68a9b42c21517d488c6b60136448eed36095f
Krisnanda <krisnanda@example.com>
2026-02-18 19:33:34 +0800

    add flight schemas

---
+++ app/Filament/Resources/Flights/Schemas/FlightForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\Flights\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
+use Filament\Schemas\Schema;
+
+class FlightForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Wizard::make([
+                    Step::make('Flight Information')
+                        ->schema([
+                            TextInput::make('flight_number')
+                                ->required()
+                                ->unique(ignoreRecord: true),
+                            Select::make('airline_id')
+                                ->relationship('airline', 'name')
+                                ->required(),
+                        ]),
+                    Step::make('Flight Segments')
+                        ->schema([
+                            Repeater::make('flight_segments')
+                            ->relationship('segments')
+                                ->schema([
+                                    TextInput::make('sequence')
+                                        ->numeric()
+                                        ->required(),
+                                    Select::make('airport_id')
+                                        ->relationship('airport', 'name')
+                                        ->required(),
+                                    DateTimePicker::make('time')
+                                        ->required(),
+                                ])
+                                ->collapsed(false)
+                                ->minItems(1),
+                        ]),
+                    Step::make('Flight Class')
+                        ->schema([
+                            // ...
+                        ]),
+                    ])->columnSpan(2),
+            ]);
+    }
+}
