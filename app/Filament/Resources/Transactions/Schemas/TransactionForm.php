fa764aca4b137df914595e81eba1c408ee16c5a1
Krisnanda <krisnanda@example.com>
2026-02-21 14:01:23 +0800

    add transaction resource

---
+++ app/Filament/Resources/Transactions/Schemas/TransactionForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources\Transactions\Schemas;
+
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+
+class TransactionForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Informasi Umum')
+                    ->schema([
+                        TextInput::make('code'),
+                        Select::make('flight_id')
+                            ->relationship('flight', 'flight_number'),
+                        Select::make('flight_class_id')
+                            ->relationship('flightClass', 'class_type'),
+                    ]),
+                Section::make('Informasi Penumpang')
+                    ->schema([
+                        TextInput::make('number_of_passengers'),
+                        TextInput::make('name'),
+                        TextInput::make('email'),
+                        TextInput::make('phone'),
+                    ]),
+                Section::make('Daftar Penumpang')
+                    ->schema([
+                        Repeater::make('passenger')
+                        ->relationship('passengers')
+                        ->schema([
+                            TextInput::make('seat.name'),
+                            TextInput::make('name'),
+                            TextInput::make('date_of_birth'),
+                            TextInput::make('nationality'),
+                        ]), 
+                    ]),
+                Section::make('Pembayaran')
+                    ->schema([
+                        TextInput::make('promo.code'),
+                        TextInput::make('promo.discount_type'),
+                        TextInput::make('promo.discount'),
+                        Select::make('payment_status'),
+                        TextInput::make('subtotal'),
+                        TextInput::make('grandtotal'),
+                    ]),
+            ]);
+    }
+}
