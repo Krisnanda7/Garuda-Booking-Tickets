36d720fce6b3f08a75ecaceb1a89fa4cf42eb8a5
Krisnanda7 <baguskrisna424@gmail.com>
2026-08-07 03:47:20 +0800

    feat: add navbar component to application layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 </head>
 
 <body>
+    @include('includes.navbar')
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="js/index.js"></script>
