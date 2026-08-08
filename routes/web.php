9bbbfeda028bb09bff8f887c51594625241f4f28
Krisnanda7 <baguskrisna424@gmail.com>
2026-08-08 21:54:17 +0800

    feat: implement flight search index page with controller and routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\FlightController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('flights', [FlightController::class, 'index'])->name('flight.index');
