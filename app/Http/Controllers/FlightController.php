4869d1d3c166ab9b6d29a44256dc1eee2b94d75f
Krisnanda7 <baguskrisna424@gmail.com>
2026-08-08 22:00:19 +0800

    refactor: inject Airline and Flight repositories into FlightController via constructor

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\AirlineRepositoryInterface;
+use App\Interfaces\FlightRepositoryInterface;
 use Illuminate\Http\Request;
 
 class FlightController extends Controller
 {
+    private AirlineRepositoryInterface $airlineRepository;
+    private FlightRepositoryInterface $flightRepository;
+
+    public function __construct(AirlineRepositoryInterface $airlineRepository, FlightRepositoryInterface $flightRepository)
+    {
+        $this->airlineRepository = $airlineRepository;
+        $this->flightRepository = $flightRepository;
+    }
+
     public function index()
     {
         return view('pages.flight.index');
