c5ee1ec40ce66f1eb2321537aeec89f43c1a5492
Krisnanda7 <baguskrisna424@gmail.com>
2026-08-11 03:12:08 +0800

    refactor: update navbar links to use named routes and define home page route name

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\FlightController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('pages.home');
 
 Route::get('flights', [FlightController::class, 'index'])->name('flight.index');
