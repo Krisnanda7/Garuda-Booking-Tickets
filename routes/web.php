b6f142ab7d749a13eee73b1c2d356904be44a238
Krisnanda7 <baguskrisna424@gmail.com>
2026-08-25 20:40:03 +0800

    fix: correct route spelling and bind flight selection buttons to the dynamic flight detail route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('flights', [FlightController::class, 'index'])->name('flight.index');
-Route::get('flight/{flightNumber}/chose-tier', [FlightController::class, 'show'])->name('flight.show');
+Route::get('flight/{flightNumber}/choose-tier', [FlightController::class, 'show'])->name('flight.show');
 
 Route::get('check-booking', [BookingController::class, 'checkBooking'])->name('booking.check');
